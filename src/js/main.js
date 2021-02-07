4a40fc4546b64cd76159ec6e73d3622330fe1e13
Viklysx <vikbelec111@yandex.ru>
2021-02-08 02:18:00 +0300

    feat: add youtube api

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import Slider from './modules/Slider';
+import PlayVideo from './modules/PlayVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+    const player = new PlayVideo('.showup .play', '.overlay');
+    player.init();
 });
