57ebf2a2a09b36c6035bf4cbbd330eb96721a0e5
Viklysx <vikbelec111@yandex.ru>
2021-02-23 02:13:07 +0300

    feat: the implementation of the secondary sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,35 @@
 import Slider from './modules/slider/Slider';
 import PlayVideo from './modules/PlayVideo';
 import MainSlider from './modules/slider/Slider-main';
+import MiniSlider from './modules/slider/Slider-mini';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({ btns: '.next', page: '.page' });
+    const slider = new MainSlider({ btns: '.next', container: '.page' });
     slider.render();
     const player = new PlayVideo('.showup .play', '.overlay');
     player.init();
+    const showUp = new MiniSlider({
+        container: '.showup__content-slider',
+        prev: '.showup__prev',
+        next: '.showup__next',
+        activeClass: 'card-active',
+        animate: true
+    });
+    showUp.init();
+    const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        prev: '.modules__info-btns .slick-prev',
+        next: '.modules__info-btns .slick-next',
+        activeClass: 'card-active',
+        animate: true,
+        autoPlay: true
+    });
+    modulesSlider.init();
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        prev: '.feed__slider .slick-prev',
+        next: '.feed__slider .slick-next',
+        activeClass: 'feed__item-active'
+    })
+    feedSlider.init();
 });
